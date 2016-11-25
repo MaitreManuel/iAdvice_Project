c9858f4200d9c064c4eb7443d1e985e333d3f2d1
Trystan <trystan.eveno@gmail.com>
2016-11-25 17:13:50 +0100

    All work except req.query.date

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ var express = require('express');
 var getvdm = require('./get-vdm');
 var app = express();
 
+var obj = JSON.parse(fs.readFileSync('./store/vdm.json', 'utf8'));
 
 app.get('/', function (req, res) {
     res.status(200).send('Server ok **** Go to http://localhost:3000/api/posts');
 });
 
 app.get('/api/posts', function(req, res){
-    res.sendFile(path.normalize(__dirname +'/store/vdm.json'));
+    if(Object.keys(req.query).length === 0) {
+        res.sendFile(path.normalize(__dirname +'/store/vdm.json'));
+    } else {
+        if(req.query.author.length > 0) {
+            obj.posts.forEach(function(element) {
+                if(req.query.author === element.author) {
+                    res.send(element);
+                }
+            });
+        }
+    }
 });
 
 var server = app.listen(3000, function () {
